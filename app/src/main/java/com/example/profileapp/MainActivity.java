e628332aa467a7a2b431bfbe6587017606cb6c8d
jouh <johnmalugu99@gmail.com>
2023-01-29 17:37:03 +0300

    updated clean

--- app/src/main/java/com/example/profileapp/MainActivity.java
+++ app/src/main/java/com/example/profileapp/MainActivity.java
@@ -1,270 +1,144 @@
 package com.example.profileapp;
 
-import android.Manifest;
-import android.app.AlertDialog;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
+
 import android.app.ProgressDialog;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.PersistableBundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.annotations.Nullable;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.UUID;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 
 public class MainActivity extends AppCompatActivity {
-
-    private FirebaseAuth firebaseAuth;
-    FirebaseUser firebaseUser;
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
-    FirebaseStorage firebaseStorage;
-    StorageReference storageReference;
-    ImageView set;
-    TextView profilepic;
-    ProgressDialog pd;
-    private static final int CAMERA_REQUEST = 100;
-    private static final int STORAGE_REQUEST = 200;
-    private static final int IMAGEPICK_GALLERY_REQUEST = 300;
-    private static final int IMAGE_PICKCAMERA_REQUEST = 400;
-    String cameraPermission[];
-    String storagePermission[];
-    Uri imageuri;
-    String profileOrCoverPhoto;
+    private static final String TAG = "TestTypo";
+    private ImageView imageView;
+    private FirebaseStorage storage;
+    Button button;
+    private boolean isImageUploaded = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
 
+        imageView = findViewById(R.id.setting_profile_image);
+        storage = FirebaseStorage.getInstance();
 
-        profilepic = findViewById(R.id.profilepic);
-        set = findViewById(R.id.setting_profile_image);
-        pd = new ProgressDialog(this);
-        pd.setCanceledOnTouchOutside(false);
-        firebaseAuth = FirebaseAuth.getInstance();
-        firebaseUser = firebaseAuth.getCurrentUser();
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        storageReference = FirebaseStorage.getInstance().getReference();
-        databaseReference = firebaseDatabase.getReference("Users");
-        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
-
-        profilepic.setOnClickListener(v -> {
-            pd.setMessage("Updating Profile Picture");
-            profileOrCoverPhoto = "image";
-            showImagePicDialog();
-        });
-
-    }
-
-
-    // checking storage permission ,if given then we can add something in our storage
-    private Boolean checkStoragePermission() {
-        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
-        return result;
-    }
-
-    // requesting for storage permission
-    private void requestStoragePermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(storagePermission, STORAGE_REQUEST);
-        }
-    }
-
-    // checking camera permission ,if given then we can click image using our camera
-    private Boolean checkCameraPermission() {
-        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
-        boolean result1 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
-        return result && result1;
-    }
-
-    // requesting for camera permission if not given
-    private void requestCameraPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(cameraPermission, CAMERA_REQUEST);
-        }
-    }
-
-
-
-    // Here we are showing image pic dialog where we will select
-    // and image either from camera or gallery
-    private void showImagePicDialog() {
-        String options[] = {"Camera", "Gallery"};
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Pick Image From");
-        builder.setItems(options, (dialog, which) -> {
-            // if access is not given then we will request for permission
-            if (which == 0) {
-                if (!checkCameraPermission()) {
-                    requestCameraPermission();
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isImageUploaded) {
+                    //if image is not uploaded, open the gallery for selection
+                    selectImage();
                 } else {
-                    pickFromCamera();
-                }
-            } else if (which == 1) {
-                if (!checkStoragePermission()) {
-                    requestStoragePermission();
-                } else {
-                    pickFromGallery();
+                    //if image is uploaded, retrieve it from Firebase storage
+                    retrieveImage();
                 }
             }
         });
-        builder.create().show();
+
+        retrieveImage();
     }
 
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case CAMERA_REQUEST: {
-                if (grantResults.length > 0) {
-                    boolean camera_accepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    boolean writeStorageaccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-                    if (camera_accepted && writeStorageaccepted) {
-                        pickFromCamera();
-                    } else {
-                        Toast.makeText(this, "Please Enable Camera and Storage Permissions", Toast.LENGTH_LONG).show();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                imageView.setImageBitmap(bitmap);
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] dataArray = baos.toByteArray();
+
+                StorageReference storageRef = storage.getReference().child("profile_pictures/" + ".jpg");
+                storageRef.putBytes(dataArray).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Upload successful");
+                            isImageUploaded = true;
+                            button.setText("Retrieve Image");
+                        } else {
+                            Log.d(TAG, "Upload failed");
+                        }
                     }
-                }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            break;
-            case STORAGE_REQUEST: {
-                if (grantResults.length > 0) {
-                    boolean writeStorageaccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    if (writeStorageaccepted) {
-                        pickFromGallery();
-                    } else {
-                        Toast.makeText(this, "Please Enable Storage Permissions", Toast.LENGTH_LONG).show();
-                    }
-                }
-            }
-            break;
         }
     }
 
-    // Here we will click a photo and then go to startactivityforresult for updating data
-    private void pickFromCamera() {
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(MediaStore.Images.Media.TITLE, "Temp_pic");
-        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Temp Description");
-        imageuri = this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
-        Intent camerIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        camerIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageuri);
-        startActivityForResult(camerIntent, IMAGE_PICKCAMERA_REQUEST);
-    }
-
-    // We will select an image from gallery
-    private void pickFromGallery() {
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
-        galleryIntent.setType("image/*");
-        startActivityForResult(galleryIntent, IMAGEPICK_GALLERY_REQUEST);
-    }
-
-    // We will upload the image from here.
-    private void uploadProfileCoverPhoto(final Uri uri) {
-        pd.show();
-
-        StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
-        ref.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+    private void retrieveImage() {
+        StorageReference storageRef = storage.getReference().child("profile_pictures/" + ".jpg");
+        final long ONE_MEGABYTE = 1024 * 1024;
+        storageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
             @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
-                while (!uriTask.isSuccessful()) ;
-
-                // We will get the url of our image using uritask
-                final Uri downloadUri = uriTask.getResult();
-                if (uriTask.isSuccessful()) {
-
-                    // updating our image url into the realtime database
-                    HashMap<String, Object> hashMap = new HashMap<>();
-                    hashMap.put(profileOrCoverPhoto, downloadUri.toString());
-                    StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
-
-                    ref.putFile(imageuri)
-
-                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-
-                                @Override
-
-                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                    
-
-                                    Toast.makeText(MainActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
-
-                                }
-
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            pd.dismiss();
-                            Toast.makeText(MainActivity.this, "Error Updating ", Toast.LENGTH_LONG).show();
-                        }
-                    });
-                } else {
-                    pd.dismiss();
-                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
-                }
+            public void onSuccess(byte[] bytes) {
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                imageView.setImageBitmap(bitmap);
+                button.setText("UPDATE PROFILE");
+                isImageUploaded = false;
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
-            public void onFailure(@NonNull Exception e) {
-                pd.dismiss();
-                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+            public void onFailure(@NonNull Exception exception) {
+                Log.d(TAG, "Retrieval failed");
             }
         });
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (data != null) {
-            if (data.getData() != null) {
-                Uri uri = data.getData(); // filepath
-                FirebaseStorage storage = FirebaseStorage.getInstance();
-                long time = new Date().getTime();
-                StorageReference reference = storage.getReference().child("Profiles").child(time + "");
-                reference.putFile(uri).addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        reference.getDownloadUrl().addOnSuccessListener(uri1 -> {
-                            String filePath = uri1.toString();
-                            HashMap<String, Object> obj = new HashMap<>();
-                            obj.put("image", filePath);
-                        });
-                    }
-                });
-                set.setImageURI(data.getData());
-                imageuri = data.getData();
-            }
-        }
     }
 }
